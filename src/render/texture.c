fff44abd877297ab1fabc23529560ee05f65bd0f
NyanCoder-1 <vityaniko@gmail.com>
2026-02-17 16:21:40 +0900

    Minor logging changes

--- src/render/texture.c
+++ src/render/texture.c
@@ -22,12 +22,12 @@ Texture textureLoadFromFile(const char *filename) {
 void textureCreateFromMemoryRawInplace(Texture_t texture, const void *data, uint32_t width, uint32_t height, enum PIXELSIZE pixelSize) {
 	// Skip if no data or empty data
 	if (!texture || !data || width == 0 || height == 0) {
-		fprintf(stderr, "Error in %s: Invalid argument\n", __func__);
+		fprintf(stderr, "Error in `%s`: Invalid argument\n", __func__);
 		return;
 	}
 
-	GLint internalFormat = 0;
-	GLenum format = 0;
+	GLint internalFormat = GL_RGBA8;
+	GLenum format = GL_RGBA;
 	switch (pixelSize) {
 	case PIXELSIZE_BYTE:
 		internalFormat = GL_R8;
@@ -46,16 +46,22 @@ void textureCreateFromMemoryRawInplace(Texture_t texture, const void *data, uint
 		format = GL_RGBA;
 		break;
 	default:
-		fprintf(stderr, "Error in %s: Unsupported pixel size\n", __func__);
+		fprintf(stderr, "Error in `%s`: Unsupported pixel size\n", __func__);
 		return;
 	}
 
+	glGenTextures(1, &texture->id);
+	glBindTexture(GL_TEXTURE_2D, texture->id);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, width, height, 0, format, GL_UNSIGNED_BYTE, data);
 }
 void textureCreateFromMemoryInplace(Texture_t texture, const uint8_t* data, size_t size) {
 	// Skip if no data or empty data
 	if (!texture || !data || size == 0) {
-		fprintf(stderr, "Error in %s: Invalid argument\n", __func__);
+		fprintf(stderr, "Error in `%s`: Invalid argument\n", __func__);
 		return;
 	}
 
@@ -77,7 +83,7 @@ void textureCreateFromMemoryInplace(Texture_t texture, const uint8_t* data, size
 		break;
 	}
 	if (width <= 0 || height <= 0 || channels <= 0 || channels >= PIXELSIZE_MAX) {
-		fprintf(stderr, "Error in %s: Invalid image data or unsupported pixel format\n", __func__);
+		fprintf(stderr, "Error in `%s`: Invalid image data or unsupported pixel format\n", __func__);
 	}
 	else {
 		textureCreateFromMemoryRawInplace(texture, img, width, height, pixelSize);
@@ -87,13 +93,13 @@ void textureCreateFromMemoryInplace(Texture_t texture, const uint8_t* data, size
 void textureLoadFromFileInplace(Texture_t texture, const char* filename) {
 	// Skip if no data or empty filename
 	if (!texture || !filename || !*filename) {
-		fprintf(stderr, "Error in %s: Invalid argument\n", __func__);
+		fprintf(stderr, "Error in `%s`: Invalid argument\n", __func__);
 		return;
 	}
 
 	uint8_t *fileData = loadAssetBinary(filename);
 	if (!fileData) {
-		fprintf(stderr, "Error in %s: Failed to load asset binary\n", __func__);
+		fprintf(stderr, "Error in `%s`: Failed to load asset binary\n", __func__);
 		return;
 	}
 	textureCreateFromMemoryInplace(texture, fileData, assetGetSize(fileData));
